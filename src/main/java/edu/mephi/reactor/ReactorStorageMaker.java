8da1825946a1600b4fa8ee82100b0ca1b06c0928
omanie <kkiirriikm@gmail.com>
2023-05-23 14:06:43 +0300

    Chain obligation fix

--- src/main/java/edu/mephi/reactor/ReactorStorageMaker.java
+++ src/main/java/edu/mephi/reactor/ReactorStorageMaker.java
@@ -1,29 +1,25 @@
 package edu.mephi.reactor;
 
-import java.io.IOException;
-
 import edu.mephi.exceptions.WrongFileFormatException;
 import edu.mephi.reactor.readers.Reader;
 import edu.mephi.reactor.readers.ReaderJSON;
 import edu.mephi.reactor.readers.ReaderXML;
 import edu.mephi.reactor.readers.ReaderYAML;
+import java.io.IOException;
 
 public class ReactorStorageMaker {
   private Reader firstReader;
-  private Reader secondReader;
-  private Reader thirdReader;
 
   public ReactorStorageMaker() {
     this.firstReader = new ReaderXML();
-    this.secondReader = new ReaderYAML();
-    this.thirdReader = new ReaderJSON();
+    Reader secondReader = new ReaderYAML();
+    Reader thirdReader = new ReaderJSON();
     firstReader.setNext(secondReader);
     secondReader.setNext(thirdReader);
   }
 
-  public ReactorStorage makeReactorStorage(String filename) 
+  public ReactorStorage makeReactorStorage(String filename)
       throws WrongFileFormatException, IOException {
     return firstReader.handleFile(filename);
   }
-  
 }
